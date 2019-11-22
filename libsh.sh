8cb2983581dc604b67154d4a88c870d792219896
Fletcher Nichol <fnichol@nichol.ca>
2019-11-22 14:31:58 -0700

    Add `setup_traps` function to libsh
    The `setup_traps` function will set up a trap for `EXIT` as well as
    common signals such as `HUP`, `INT`, `QUIT`, `ALRM`, and `TERM`. This
    implementation will use the same "cleanup function" for all traps which
    may or may not be the desired behavior depending on the script's
    implementation needs.
    
    Note that the Public Domain Korn Shell (i.e. pdksh) has 3 pending signal
    tests that are skipped. While it appears that the implementation works
    and the method of signaling a backgrounded script works, it has been a
    challenge to get the unit tests to pass.
    
    Signed-off-by: Fletcher Nichol <fnichol@nichol.ca>

--- libsh.sh
+++ libsh.sh
@@ -379,6 +379,65 @@ section() {
   unset _msg
 }
 
+# Sets up traps for `EXIT` and common signals with the given cleanup function.
+#
+# In addition to `EXIT`, the `HUP`, `INT`, `QUIT`, `ALRM`, and `TERM` signals
+# are also covered.
+#
+# This implementation was based on a very nice, portable signal handling thread
+# thanks to: https://unix.stackexchange.com/a/240736
+#
+# * `@param [String]` name of function to run with traps
+#
+# # Examples
+#
+# Basic usage with a simple "hello world" cleanup function:
+#
+# ```sh
+# hello_trap() {
+#   echo "Hello, trap!"
+# }
+#
+# setup_traps hello_trap
+# ```
+#
+# If the cleanup is simple enough to be a one-liner, you can provide the
+# command as the single argument:
+#
+# ```sh
+# setup_traps "echo 'Hello, World!'"
+# ```
+setup_traps() {
+  local _trap_fun
+  _trap_fun="$1"
+
+  local _sig
+  for _sig in HUP INT QUIT ALRM TERM; do
+    trap "
+      $_trap_fun
+      trap - $_sig EXIT
+      kill -s $_sig "'"$$"' "$_sig"
+  done
+
+  if [ -n "${ZSH_VERSION:-}" ]; then
+    # Zsh uses the `EXIT` trap for a function if declared in a function.
+    # Instead, use the `zshexit()` hook function which targets the exiting of a
+    # shell interpreter. Additionally, a function in Zsh is not a closure over
+    # outer variables, so we'll use `eval` to construct the function body
+    # containing the cleanup function to invoke.
+    #
+    # See:
+    # * https://stackoverflow.com/a/22794374
+    # * http://zsh.sourceforge.net/Doc/Release/Functions.html#Hook-Functions
+    eval "zshexit() { eval '$_trap_fun'; }"
+  else
+    # shellcheck disable=SC2064
+    trap "$_trap_fun" EXIT
+  fi
+
+  unset _trap_fun _sig
+}
+
 # Removes any tracked files registered via [`cleanup_file`].
 #
 # * `@return 0` whether or not an error has occurred
--- tests/libsh_test.sh
+++ tests/libsh_test.sh
@@ -65,7 +65,7 @@ testCleanupFileNoVar() {
 
 testDieStripAnsi() {
   printf -- '\nxxx I give up\n\n' >"$expected"
-  run_in_sh_script die 'I give up'
+  run_with_sh_script die 'I give up'
 
   stripAnsi <"$stderr" >"$actual"
 
@@ -79,7 +79,7 @@ testDieStripAnsi() {
 testDieAnsi() {
   printf -- '\n\033[1;31;40mxxx \033[1;37;40mI give up\033[0m\n\n' >"$expected"
   export TERM=xterm
-  run_in_sh_script die 'I give up'
+  run_with_sh_script die 'I give up'
 
   assertFalse 'fail did not fail' "$return_status"
   # Shell string equals has issues with ANSI escapes, so let's use `cmp` to
@@ -128,7 +128,7 @@ testNeedCmdPresent() {
 }
 
 testNeedCmdMissing() {
-  run_in_sh_script need_cmd __not_a_great_chance_this_will_exist__
+  run_with_sh_script need_cmd __not_a_great_chance_this_will_exist__
 
   assertFalse 'need_cmd succeeded' "$return_status"
   assertStderrStripAnsiContains 'xxx Required command'
@@ -253,6 +253,77 @@ testSectionAnsi() {
   assertStderrNull
 }
 
+testSetupTrapsSimple() {
+  run_in_sh_script <<'EOF'
+    echo "start"
+    setup_traps "echo 'trap fired'"
+    echo "end"
+EOF
+
+  assertTrue 'setup_traps failed' "$return_status"
+  assertStdoutEquals "start
+end
+trap fired"
+  assertStderrNull
+}
+
+testSetupTrapsHUP() {
+  # TODO: Skip for pdksh--the setup script isn't handling the signal correctly
+  # despite the trap implementation working with the sh_script.
+  if echo "${KSH_VERSION:-}" | grep -q "PD KSH"; then
+    return
+  fi
+
+  signalScript HUP
+
+  assertTrue 'setup_traps failed' "$return_status"
+  if [ -n "${ZSH_VERSION:-}" ]; then
+    # Zsh will invoke the trap on `HUP` and again when the process exits via
+    # the `zshexit()` hook
+    assertStdoutEquals "start
+trap fired
+trap fired"
+  else
+    assertStdoutEquals "start
+trap fired"
+  fi
+  # stderr may contain a message about the terminated process
+}
+
+testSetupTrapsALRM() {
+  # TODO: Skip for pdksh--the setup script isn't handling the signal correctly
+  # despite the trap implementation working with the sh_script.
+  if echo "${KSH_VERSION:-}" | grep -q "PD KSH"; then
+    return
+  fi
+
+  signalScript ALRM
+
+  assertTrue 'setup_traps failed' "$return_status"
+  assertStdoutEquals "start
+trap fired"
+  # stderr may contain a message about the terminated process
+}
+
+testSetupTrapsTERM() {
+  # TODO: Skip for pdksh--the setup script isn't handling the signal correctly
+  # despite the trap implementation working with the sh_script.
+  if echo "${KSH_VERSION:-}" | grep -q "PD KSH"; then
+    return
+  fi
+
+  signalScript TERM
+
+  assertTrue 'setup_traps failed' "$return_status"
+  assertStdoutEquals "start
+trap fired"
+  # stderr may contain a message about the terminated process
+}
+
+# testSetupTrapsINT - does not terminate script with test suite so is skipped
+
+# testSetupTrapsQUIT - does not terminate script with test suite so is skipped
+
 testTrapCleanupFiles() {
   local alpha bravo charlie
   __CLEANUP_FILES__="$(mktemp_file)"
@@ -312,6 +383,16 @@ createGitRepo() {
 
   short_sha="$(git show -s --format=%h)"
   long_sha="$(git show -s --format=%H)"
+}
+
+signalScript() {
+  run_in_sh_script_and_signal "$1" <<'EOF'
+    echo "start"
+    setup_traps "echo 'trap fired'"
+    sleep 10 &
+    wait $!
+    echo "end"
+EOF
 }
 
 shell_compat "$0"
