799eab912f2ff0a2f634712aee39f5a878b064a3
Fletcher Nichol <fnichol@nichol.ca>
2020-05-13 13:23:10 -0600

    Add `indent` function to libsh
    Signed-off-by: Fletcher Nichol <fnichol@nichol.ca>

--- libsh.sh
+++ libsh.sh
@@ -249,6 +249,58 @@ download() {
   return 1
 }
 
+# Indents the output from a command while preserving the command's exit code.
+#
+# In minimal/POSIX shells there is no support for `set -o pipefail` which means
+# that the exit code of the first command in a shell pipeline won't be
+# addressable in an easy way. This implementation uses a temp file to ferry the
+# original command's exit code from a subshell back into the main function. The
+# output can be aligned with a pipe to `sed` as before but now we have an
+# implementation which mimicks a `set -o pipefail` which should work on all
+# Bourne shells. Note that the `set -o errexit` is disabled during the
+# command's invocation so that its exit code can be captured.
+#
+# Based on implementation from: https://stackoverflow.com/a/54931544
+#
+# * `@param [String[]]` command and arguments
+# * `@return` the exit code of the command which was executed
+#
+# # Notes
+#
+# In order to preserve the output order of the command, the `stdout` and
+# `stderr` streams are combined, so the command will not emit its `stderr`
+# output to the caller's `stderr` stream.
+#
+# # Examples
+#
+# Basic usage:
+#
+# ```sh
+# indent cat /my/file
+# ```
+indent() {
+  local _ecfile _ec _orig_flags
+
+  need_cmd cat
+  need_cmd rm
+  need_cmd sed
+
+  _ecfile="$(mktemp_file)"
+
+  _orig_flags="$-"
+  set +e
+  {
+    "$@" 2>&1
+    echo "$?" >"$_ecfile"
+  } | sed 's/^/       /'
+  set "-$(echo "$_orig_flags" | sed s/s//g)"
+  _ec="$(cat "$_ecfile")"
+  rm -f "$_ecfile"
+
+  unset _ecfile _orig_flags
+  return "${_ec:-5}"
+}
+
 # Prints an informational, detailed step to standard out.
 #
 # * `@param [String]` informational text
--- tests/libsh_test.sh
+++ tests/libsh_test.sh
@@ -306,6 +306,50 @@ testDownloadNoWgetOrCurlOrFtp() {
   assertStderrNull
 }
 
+testIndent() {
+  run indent echo 'hello, world'
+
+  assertTrue 'indent failed' "$return_status"
+  assertStdoutStripAnsiEquals '       hello, world'
+  assertStderrNull
+}
+
+testIndentPropagatesCommandExitCode() {
+  touch "$tmppath/exiter"
+  chmod 0755 "$tmppath/exiter"
+  {
+    echo '#!/usr/bin/env sh'
+    echo 'exit 19'
+  } >>"$tmppath/exiter"
+
+  run indent "$tmppath/exiter"
+
+  assertEquals "exit code isn't equal" "19" "$return_status"
+}
+
+testIndentCombinesOutputStreams() {
+  printf -- '       stdout1\n' >"$expected"
+  printf -- '       stderr1\n' >>"$expected"
+  printf -- '       stdout2\n' >>"$expected"
+  touch "$tmppath/streamer"
+  chmod 0755 "$tmppath/streamer"
+  {
+    echo '#!/usr/bin/env sh'
+    echo 'echo "stdout1"'
+    echo 'echo "stderr1" >&2'
+    echo 'echo "stdout2"'
+    echo 'exit 0'
+  } >>"$tmppath/streamer"
+
+  run indent "$tmppath/streamer"
+
+  assertTrue 'indent failed' "$return_status"
+  # Shell string equals has issues trailing newlines, so let's use `cmp` to
+  # compare byte by byte
+  assertTrue 'stdout not equal' "cmp '$expected' '$stdout'"
+  assertStderrNull
+}
+
 testInfoStripAnsi() {
   run info 'something is happening'
 
